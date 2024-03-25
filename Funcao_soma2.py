361e4e94a6939cfcb2932efda8a3410a7fc8b0ac
caio peliz <cardosojoao418@gmail.com>
2024-03-24 23:35:26 -0300

    codigos do 2 semestre de faculdade perdido pelo notebook

---
+++ Funcao_soma2.py
@@ -0,0 +1,12 @@
+def somadosnum(n):
+    if n <= 0:
+        return 0
+    soma = 0
+    for i in range(1, n + 1):
+        soma += i
+    return soma
+
+
+n = int(input("digite um numero: "))
+resultado = somadosnum(n)
+print(f'a soma de todos os inteiros positivos ate {n} é {resultado}')
--- funcao_soma.py
+++ funcao_soma.py
@@ -1,5 +1,23 @@
-def soma(a,b):
-    resultado = a + b
-    return resultado
-print(soma)
+def imprime(s):
+    print(s)
+    print(id(s))
 
+
+def soma(x, y):
+    return (x+y)
+
+
+def concaten(x, y):
+    z = x + y
+    return z
+
+
+z = soma(6, 7)
+
+a = "Caio"
+b = "Peliz"
+w = concaten(a, b)
+
+imprime(w)
+print(id(w))
+imprime(z)
